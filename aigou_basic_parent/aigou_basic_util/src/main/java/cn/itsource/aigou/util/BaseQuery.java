803306db2bb5a9fb11ca42ca2e3135fe2bcb79a8
admin <admin@qq.com>
2019-03-30 13:17:27 +0800

    BaseQuery

---
+++ aigou_basic_parent/aigou_basic_util/src/main/java/cn/itsource/aigou/util/BaseQuery.java
@@ -0,0 +1,32 @@
+package cn.itsource.aigou.util;
+
+public class BaseQuery {
+
+    private Integer page = 0;
+    private Integer rows = 10;
+    private String q;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+    public String getQ() {
+        return q;
+    }
+
+    public void setQ(String q) {
+        this.q = q;
+    }
+}
